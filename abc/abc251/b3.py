8615fa203dde2b8737ffb24560f1e0cf145f86fd
Musashi Okuoka <okuoka.engineer@gmail.com>
2022-08-22 11:10:45 +0900

    解いた

---
+++ abc/abc251/b3.py
@@ -0,0 +1,14 @@
+N, W = map(int, input().split())
+a = list(map(int, input().split()))
+
+s = set()
+
+for i in range(N):
+    s.add(a[i])
+    for j in range(i+1, N):
+        s.add(a[i] + a[j])
+        for k in range(j+1, N):
+            s.add(a[i] + a[j] + a[k])
+
+nums = list(filter(lambda v: v <= W, s))
+print(len(nums))
